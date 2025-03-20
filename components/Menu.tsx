a53092ae2a3f72a9f0d4a65204e2b9c82cbac4b9
Kicknee <daniszewski21@gmail.com>
2025-03-20 17:50:35 +0100

    add icons to the menu elements

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,36 +1,47 @@
 import React from "react";
 import Link from "next/link";
+import {
+  IoHomeOutline,
+  IoUmbrellaOutline,
+  IoPeopleOutline,
+  IoTodayOutline,
+  IoChatboxEllipsesOutline,
+  IoNotificationsOutline,
+  IoPersonCircleOutline,
+  IoSettingsOutline,
+  IoLogOutOutline,
+} from "react-icons/io5";
 const menuItems = [
   {
     title: "MENU",
     items: [
       {
-        icon: "",
+        icon: <IoHomeOutline className="menu-icon" />,
         label: "Home",
         href: "/",
       },
       {
-        icon: "",
+        icon: <IoUmbrellaOutline className="menu-icon" />,
         label: "Babysitters",
         href: "/babysitters",
       },
       {
-        icon: "",
+        icon: <IoPeopleOutline className="menu-icon" />,
         label: "Parents",
         href: "/parents",
       },
       {
-        icon: "",
+        icon: <IoTodayOutline className="menu-icon" />,
         label: "Events",
         href: "/events",
       },
       {
-        icon: "",
+        icon: <IoChatboxEllipsesOutline className="menu-icon" />,
         label: "Messages",
         href: "/messages",
       },
       {
-        icon: "",
+        icon: <IoNotificationsOutline className="menu-icon" />,
         label: "Announcements",
         href: "/announcements",
       },
@@ -39,9 +50,21 @@ const menuItems = [
   {
     title: "OTHER",
     items: [
-      { icon: "", label: "Profile", href: "/profile" },
-      { icon: "", label: "Settings", href: "/settings" },
-      { icon: "", label: "Logout", href: "/logout" },
+      {
+        icon: <IoPersonCircleOutline className="menu-icon" />,
+        label: "Profile",
+        href: "/profile",
+      },
+      {
+        icon: <IoSettingsOutline className="menu-icon" />,
+        label: "Settings",
+        href: "/settings",
+      },
+      {
+        icon: <IoLogOutOutline className="menu-icon" />,
+        label: "Logout",
+        href: "/logout",
+      },
     ],
   },
 ];
@@ -54,8 +77,14 @@ export default function Menu() {
             {menu.title}
           </span>
           {menu.items.map((item) => (
-            <Link key={item.href} href={item.href}>
-              {item.label}
+            <Link
+              key={item.href}
+              href={item.href}
+              //   className="[&>.lucide]:size-[50px]"
+              className="flex items-center justify-center lg:justify-start gap-2 text-gray-500 py-2 [&>.menu-icon]:size-[20px]"
+            >
+              {item.icon}
+              <p className="hidden lg:block">{item.label}</p>
             </Link>
           ))}
         </div>
