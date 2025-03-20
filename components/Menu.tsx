f593e38459b0b01237fdd63579dc89e99c65c0d0
Kicknee <daniszewski21@gmail.com>
2025-03-20 16:07:33 +0100

    display all items in menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,65 @@
 import React from "react";
-
+import Link from "next/link";
+const menuItems = [
+  {
+    title: "MENU",
+    items: [
+      {
+        icon: "",
+        label: "Home",
+        href: "/",
+      },
+      {
+        icon: "",
+        label: "Babysitters",
+        href: "/babysitters",
+      },
+      {
+        icon: "",
+        label: "Parents",
+        href: "/parents",
+      },
+      {
+        icon: "",
+        label: "Events",
+        href: "/events",
+      },
+      {
+        icon: "",
+        label: "Messages",
+        href: "/messages",
+      },
+      {
+        icon: "",
+        label: "Announcements",
+        href: "/announcements",
+      },
+    ],
+  },
+  {
+    title: "OTHER",
+    items: [
+      { icon: "", label: "Profile", href: "/profile" },
+      { icon: "", label: "Settings", href: "/settings" },
+      { icon: "", label: "Logout", href: "/logout" },
+    ],
+  },
+];
 export default function Menu() {
-  return <div>Menu</div>;
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((menu) => (
+        <div className="flex flex-col gap-2" key={menu.title}>
+          <span className="hidden lg:block text-gray-400 font-light">
+            {menu.title}
+          </span>
+          {menu.items.map((item) => (
+            <Link key={item.href} href={item.href}>
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
