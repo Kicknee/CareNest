96971ee84da8533e463fc03806ecb6b7add6c87b
Kicknee <daniszewski21@gmail.com>
2025-03-20 17:49:36 +0100

    fix children type

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
-import React, { PropsWithChildren } from "react";
+import React from "react";
 import Image from "next/image";
+import Menu from "@/components/Menu";
 
-export default function DashboardLayout({ children }: PropsWithChildren) {
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div className="flex justify-between h-screen max-w-[1300px] mx-auto">
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-amber-100 p-3">
@@ -10,6 +15,7 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
           <Image src="logo.svg" alt="logo" width={53} height={53} />
           <span className="hidden lg:block">CareNest</span>
         </Link>
+        <Menu />
       </div>
       {children}
       {/* <div className="w-[86%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-amber-300">right</div> */}
