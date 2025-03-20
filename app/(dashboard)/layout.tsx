b8741d5f6bc328a36141ec3f59fd4786f689b27b
Kicknee <daniszewski21@gmail.com>
2025-03-20 15:42:52 +0100

    create menu and navbar components

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 import React, { PropsWithChildren } from "react";
+import Image from "next/image";
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex justify-between h-screen max-w-[1300px] mx-auto">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-amber-100 ">
-        left
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-amber-100 p-3">
+        <Link href="/" className="flex items-center lg:justify-start gap-2">
+          <Image src="logo.svg" alt="logo" width={53} height={53} />
+          <span className="hidden lg:block">CareNest</span>
+        </Link>
       </div>
       {children}
       {/* <div className="w-[86%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-amber-300">right</div> */}
