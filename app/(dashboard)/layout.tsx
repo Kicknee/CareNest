f6d032e698d17dc5e89219ebe76b98806d099997
Kicknee <daniszewski21@gmail.com>
2025-03-26 18:33:47 +0100

    change layout propotions for various screen size

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,15 +11,15 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="flex h-screen max-w-[1300px] mx-auto">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-secondary p-3 text-white">
+      <div className="w-[16%] md:w-[10%] lg:w-[16%] xl:w-[14%] bg-secondary p-3 text-white">
         <Link href="/" className="flex items-center lg:justify-start gap-2">
           <Image src="/logo_s.png" alt="logo" width={53} height={53} />
-          <span className="hidden lg:block text-[21px]">CareNest</span>
+          <span className="hidden lg:block text-[21px]">Carenest</span>
         </Link>
         <Menu />
       </div>
 
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-y-scroll ">
+      <div className="w-[84%] md:w-[90%] lg:w-[84%] xl:w-[86%] overflow-y-scroll ">
         <Navbar />
         {children}
       </div>
