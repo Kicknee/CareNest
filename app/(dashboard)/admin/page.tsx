a4772b9ad1ee007a37bdefd19c58e199acf29cf8
Kicknee <daniszewski21@gmail.com>
2025-03-26 19:15:02 +0100

    create page structure for mobile and desktop

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,5 +1,21 @@
+import UserCard from "@/components/UserCard";
 import React from "react";
 
 export default function AdminPage() {
-  return <div>AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* CARD */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type={"babysitter"} />
+          <UserCard type={"parent"} />
+        </div>
+        {/* MIDDLE CHARTS */}
+        <div></div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">RIGHT</div>
+    </div>
+  );
 }
