fe00acbbb1cb65136b77563c861463d7ca7aa941
Kicknee <daniszewski21@gmail.com>
2025-03-26 18:32:39 +0100

    add account info in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { IoPersonCircleOutline } from "react-icons/io5";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-end p-5">
+      <div className="flex gap-2">
+        <div className="flex flex-col text-end">
+          <span className="text-lg">John Doe</span>
+          <span className="text-xs text-gray-500">Admin</span>
+        </div>
+        <IoPersonCircleOutline className="size-[41px] text-accent" />
+      </div>
+    </div>
+  );
 }
