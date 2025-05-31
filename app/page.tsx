ab1d2f2ef3b1091eb7ce6fee1103e0ba027df7dd
Kicknee <daniszewski21@gmail.com>
2025-05-31 23:56:29 +0200

    create grid for middle section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -27,7 +28,7 @@ export default function Home() {
     <div className="text-3xl bg-gradient-to-b from-primary to-50% to-secondary h-screen p-8">
       <header className="flex justify-between text-white">
         <Link href="/" className="text-6xl">
-          Carenest
+          <h1>Carenest</h1>
         </Link>
         <nav className="flex flex-1 justify-between items-center px-9">
           {menuItems.map((item) => (
@@ -47,6 +48,14 @@ export default function Home() {
           </Link>
         </nav>
       </header>
+      <div className="w-75 grid grid-cols-3 grid-rows-3">
+        <Image
+          src="/logo_m.png"
+          alt="Logo"
+          fill
+          className="row-span-2 col-span-2"
+        />
+      </div>
     </div>
   );
 }
