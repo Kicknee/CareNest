58ea30c79a8a68d759966ced6e5efe4361ac2801
Kicknee <daniszewski21@gmail.com>
2025-06-02 19:24:18 +0200

    create nanny card component and import to home page

---
+++ components/NannyCard.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { CiStar } from "react-icons/ci";
+
+export default function NannyCard() {
+  return (
+    <div className="bg-white rounded-2xl p-3 w-[170px] h-[270px] flex flex-col items-center justify-between text-black gap-2">
+      <Image src="/logo_m.png" alt="Logo" width={75} height={75} />
+      <p className="text-base font-semibold">Chido Asun</p>
+      <p className="capitalize text-sm">Warsaw</p>
+      <div>
+        <p className="capitalize text-xs">{"Experience: >4 years"}</p>
+        <p className="capitalize text-xs mt-1">PLN 35.00/hr</p>
+      </div>
+      <div className="flex text-xs font-semibold items-center gap-1">
+        <CiStar size={21} />
+        <CiStar size={21} />
+        <CiStar size={21} />
+        <CiStar size={21} />
+        <CiStar size={21} />
+        (23)
+      </div>
+      <button className="rounded-4xl px-5 py-1.5 text-sm bg-accent text-white">
+        Book Now
+      </button>
+    </div>
+  );
+}
