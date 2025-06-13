156c2850883dcf231517f7af755b45de10de9c18
Kicknee <daniszewski21@gmail.com>
2025-06-13 16:27:53 +0200

    add snap feature when scrooling nanny cards

--- app/page.tsx
+++ app/page.tsx
@@ -27,67 +27,107 @@ export default function Home() {
     },
   ];
   return (
-    <div className="flex h-screen flex-col items-center p-8 text-3xl text-white">
-      <header className="grid grid-cols-[1fr_2fr] grid-rows-[60px_30px_150px_45px]">
-        <Image src="/logo_m.png" alt="Logo" width={90} height={90} />
-        <Link href="/" className="text-5xl md:text-6xl">
-          <h1>Carenest</h1>
-        </Link>
-        <div className="col-start-2 row-[2_/_span_1] flex items-center justify-center gap-1 text-2xl">
-          <p className="text-[80%]">Find your nanny</p>
-          <IoMenu size={30} />
+    <main className="flex w-full max-w-screen flex-col items-center p-8 text-3xl text-white">
+      <header className="flex w-full items-center justify-center gap-2 md:items-baseline md:justify-around">
+        <div className="relative size-[90px] md:hidden">
+          <Image src="/logo_m.png" alt="Logo" fill />
         </div>
-        <nav className="hidden flex-1 items-center justify-between px-9 md:flex">
+        <div className="md:flex">
+          <Link href="/" className="md:text-[min(10vw, 70px)] text-5xl">
+            <h1>Carenest</h1>
+          </Link>
+          <div className="flex items-center justify-center gap-1 text-2xl md:hidden">
+            <p className="text-[80%] md:text-6xl">Find your nanny</p>
+            <IoMenu size={30} />
+          </div>
+        </div>
+
+        <nav className="hidden flex-1 items-center justify-around md:flex">
           {menuItems.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className={`text-xl ${item.href === "/log-in" && "underline"}`}
+              className={`text-lg ${item.href === "/log-in" && "underline"}`}
             >
               {item.label}
             </Link>
           ))}
           <Link
             href="/sign-up"
-            className="bg-secondary rounded-[30px] px-7 py-2 text-xl"
+            className="bg-secondary rounded-[30px] px-7 py-2 text-lg"
           >
             Sign up
           </Link>
         </nav>
-        <button className="bg-accent col-2 row-3 hidden place-self-start self-center rounded-4xl px-7 py-2 text-2xl">
-          Book Now
-        </button>
-        {/*searchbar*/}
-        <Image
-          src="/bed_bg.png"
-          alt="bed icon"
-          width={90}
-          height={90}
-          className="col-end-2 row-[3_/_span_1] self-end"
-        />
-        <div className="relative col-span-full row-4 flex size-full self-end overflow-hidden rounded-4xl bg-white">
-          <input type="text" className="w-[80%]" />
-          <button className="bg-accent w-[20%] px-4">
-            <IoSearch size={35} />
+      </header>
+      <section className="mt-10 grid w-full grid-cols-[1fr_2fr_1fr]">
+        <div className="relative hidden size-[200px] justify-self-center md:block">
+          <Image src="/logo_m.png" alt="Logo" fill />
+        </div>
+        <div className="col-start-2 flex flex-col items-center justify-around">
+          <p className="hidden text-[80%] md:block md:text-[min(10vw,42px)]">
+            Find your nanny
+          </p>
+          <button className="bg-accent col-2 row-3 self-center rounded-4xl px-7 py-2 text-lg md:text-2xl">
+            Book Now
           </button>
+        </div>
+        <div className="relative -z-3 hidden size-[min(20vw,200px)] md:block">
+          <Image src="/sun_full_bg.png" fill alt="sun icon" />
         </div>
         <Image
           src="/sun_bg.png"
           width={90}
           height={90}
           alt="sun icon"
-          className="absolute top-2/15 right-0 -z-3"
+          className="absolute top-2/15 right-0 -z-3 md:hidden"
         />
-      </header>
+        {/*searchbar*/}
+        <div className="col-span-full row-2 flex flex-col items-center">
+          <div className="relative size-[80px] -translate-x-[120%] md:size-[130px] md:translate-x-[220%] md:self-center">
+            <Image src="/bed_bg.png" alt="bed icon" fill />
+          </div>
+          <div className="flex h-13 overflow-hidden rounded-4xl bg-white md:w-3xl">
+            <input type="text" className="w-[85%] md:w-[75%]" />
+            <button className="bg-accent flex w-[15%] items-center justify-around px-4 md:w-[25%]">
+              <p className="hidden text-[25px] md:block">Search</p>
+              <IoSearch size={30} />
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="flex w-screen flex-col items-center">
+        {/*tiles section */}
+        <p className="my-10 text-3xl md:text-5xl">Browse</p>
+        <div className="mt-3 flex w-[80%] snap-x flex-wrap gap-5 overflow-scroll md:w-screen md:flex-nowrap md:justify-start md:px-5">
+          <div className="snap-start">
+            <NannyCard />
+          </div>
+          <div className="snap-start">
+            <NannyCard />
+          </div>
+          <div className="snap-start">
+            <NannyCard />
+          </div>
+          <div className="snap-start">
+            <NannyCard />
+          </div>
 
-      <p className="mt-10 text-3xl">Browse</p>
-      <div className="mt-3 flex w-screen flex-wrap justify-center gap-5">
-        <NannyCard />
-        <NannyCard />
-        <NannyCard />
-        <NannyCard />
-      </div>
+          <div className="snap-start">
+            <NannyCard />
+          </div>
+          <div className="snap-start">
+            <NannyCard />
+          </div>
+          <div className="snap-start">
+            <NannyCard />
+          </div>
+          <div className="snap-start">
+            <NannyCard />
+          </div>
+        </div>
+      </section>
       <div className="from-primary to-secondary min-h-screen; fixed top-0 left-0 -z-10 size-full bg-gradient-to-b to-50%"></div>
-    </div>
+    </main>
   );
 }
--- components/NannyCard.tsx
+++ components/NannyCard.tsx
@@ -3,10 +3,14 @@ import { AiFillStar } from "react-icons/ai";
 
 export default function NannyCard() {
   return (
-    <div className="flex h-[270px] w-[170px] flex-col items-center justify-between gap-2 rounded-2xl bg-white p-3 text-black">
-      <Image src="/logo_m.png" alt="Logo" width={75} height={75} />
-      <p className="text-base font-semibold">Chido Asun</p>
-      <p className="text-sm capitalize">Warsaw</p>
+    <div className="flex h-[270px] w-[170px] flex-col items-center justify-between gap-2 rounded-2xl bg-white p-3 text-black md:h-[360px] md:w-[230px]">
+      <div className="relative size-[75px] md:size-[95px]">
+        <Image src="/logo_m.png" alt="Logo" fill />
+      </div>
+      <div className="text-center">
+        <p className="text-base font-semibold">Chido Asun</p>
+        <p className="text-sm capitalize">Warsaw</p>
+      </div>
       <div>
         <p className="text-xs capitalize">{"Experience: >4 years"}</p>
         <p className="mt-1 text-xs capitalize">PLN 35.00/hr</p>
@@ -19,7 +23,7 @@ export default function NannyCard() {
         <AiFillStar size={20} className="text-gray-200" />
         (23)
       </div>
-      <button className="bg-accent rounded-4xl px-5 py-1.5 text-sm text-white">
+      <button className="bg-accent rounded-4xl px-5 py-1.5 text-sm text-white md:px-6 md:py-2">
         Book Now
       </button>
     </div>
