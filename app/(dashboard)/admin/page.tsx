1d9be0dbd4525c4d014fbd3848f2f00e98e30760
Kicknee <daniszewski21@gmail.com>
2025-07-11 16:01:51 +0200

    change style for info tiles

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -3,19 +3,20 @@ import React from "react";
 
 export default function AdminPage() {
   return (
-    <div className="p-4 flex gap-4 flex-col md:flex-row">
+    <div className="flex flex-col gap-4 p-4 md:flex-row">
       {/* LEFT */}
-      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+      <div className="flex w-full flex-col gap-8 lg:w-2/3">
         {/* CARD */}
-        <div className="flex gap-4 justify-between flex-wrap">
+        <div className="flex flex-wrap justify-between gap-4">
           <UserCard type={"babysitter"} />
           <UserCard type={"parent"} />
+          <UserCard type={"kid"} />
         </div>
         {/* MIDDLE CHARTS */}
         <div></div>
       </div>
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3 flex flex-col gap-8">RIGHT</div>
+      <div className="flex w-full flex-col gap-8 lg:w-1/3">RIGHT</div>
     </div>
   );
 }
--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function UserCard({ type }: { type: string }) {
   return (
-    <div className="odd:bg-carePurple even:bg-secondary min-w-[130px] flex-1 rounded-2xl p-4">
+    <div className="min-w-[170px] flex-1 rounded-2xl bg-white p-4 shadow-xl">
       <h2 className="text-sm capitalize">{type}s</h2>
       <h1 className="my-4 text-2xl font-semibold">1,234</h1>
     </div>
