e83f5ec3ae779aee7088a72271b679a426122c24
Kicknee <daniszewski21@gmail.com>
2025-10-20 20:07:11 +0200

    add hover effect to menu items

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -45,7 +45,10 @@ export default function Menu() {
   return (
     <div className="mt-4 text-sm">
       {menuItems.map((menu) => (
-        <div className="flex flex-col gap-2 md:font-semibold" key={menu.title}>
+        <div
+          className="flex w-full flex-col gap-2 md:font-semibold"
+          key={menu.title}
+        >
           <span
             className={cn("my-4 text-[16px] text-transparent lg:text-white", {
               "mt-[30px]": menu.title === "OTHER",
@@ -58,7 +61,7 @@ export default function Menu() {
             <Link
               key={item.href}
               href={item.href}
-              className="flex items-center justify-center gap-2 py-2 text-white lg:justify-start [&>.menu-icon]:size-[30px]"
+              className="hover:bg-hover my-2 flex items-center justify-center gap-2 px-3 py-1 text-white hover:rounded-4xl hover:shadow-2xl lg:justify-start [&>.menu-icon]:size-[30px]"
             >
               {item.icon}
               <p className="hidden text-[20px] lg:block">{item.label}</p>
