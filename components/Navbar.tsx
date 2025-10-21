cd32f39160429dd2025ab32126aa0fb5d69603f8
Kicknee <daniszewski21@gmail.com>
2025-10-21 23:09:49 +0200

    change color and font weight of user name and role

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import { IoPersonCircleOutline } from "react-icons/io5";
 
 export default function Navbar() {
   return (
-    <div className="bg-secondary sticky top-0 z-50 flex w-full items-center justify-end p-5 shadow-lg">
-      <div className="flex gap-2">
-        <div className="flex flex-col text-end">
-          <span className="text-lg text-white">John Doe</span>
-          <span className="text-xs text-gray-500">Admin</span>
+    <div className="bg-secondary sticky top-0 z-50 flex w-full items-center justify-between p-5 shadow-lg">
+      <Link href="/" className="flex items-center gap-2 lg:justify-start">
+        <Image src="/logo_s.png" alt="logo" width={53} height={53} />
+        <span className="hidden text-[21px] font-semibold text-white lg:block">
+          Carenest
+        </span>
+      </Link>
+      <div className="flex gap-2 text-white">
+        <div className="flex flex-col text-end font-bold">
+          <span className="text-lg">John Doe</span>
+          <span className="text-xs">Admin</span>
         </div>
-        <IoPersonCircleOutline className="text-accent size-[41px]" />
+        <IoPersonCircleOutline className="size-[41px]" />
       </div>
     </div>
   );
