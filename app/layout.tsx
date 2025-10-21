bcabb7110defb9af7fe913ae71e7a955a1f4b894
Kicknee <daniszewski21@gmail.com>
2025-10-21 23:11:01 +0200

    add weight options for Montserrat

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
-  weight: "400",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
